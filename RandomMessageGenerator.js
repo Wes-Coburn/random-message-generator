d1396492054c252a9fe2bf2118f5a28bac0a5a63
weslex555 <wgcoburn@outlook.com>
2023-04-06 14:03:24 -0700

    Remove HelloWorld.txt

--- RandomMessageGenerator.js
+++ RandomMessageGenerator.js
@@ -7,10 +7,18 @@ const rl = createInterface({
 
 rl.question('\nWhat do you want to do today? ', Response);
 
+const chatBot = {
+    intros : ['Hmm... I have an idea, you could', 'Alright, here\'s what we\'re gonnd do..',
+    'Quit stalling, you need to', 'How about you'],
+    activities : ['eat something', 'join a club', 'take a walk', 'call a friend'],
+    outros : ['remember to have fun', 'no time like the present', 'take your time, there\'s no rush'],
+    randomPhrase : phrases => phrases[Math.floor(Math.random() * phrases.length)],
+}
+
 function Response(answer) {
     let response;
     switch(answer) {
-        case answer.match(/don't know|dont know|not sure|no idea/)?.input:
+        case answer.match(/don't know|dont know|not sure|no idea|\?/)?.input:
             response = 'The world is your oyster!';
             break;
         default:
@@ -21,16 +29,10 @@ function Response(answer) {
     Suggestions();
 }
 
-const intros = ['Hmm... I have an idea, you could', 'Alright, here\'s what we\'re gonnd do..',
-    'Quit stalling, you need to', 'How about you'];
-const activities = ['eat something', 'join a club', 'take a walk', 'call a friend'];
-const outros = ['remember to have fun', 'no time like the present', 'take your time, there\'s no rush'];
-const randomPhrase = phrases => phrases[Math.floor(Math.random() * phrases.length)];
-
 function Suggestions() {
-    const intro = randomPhrase(intros);
-    const activity = randomPhrase(activities);
-    const outro = randomPhrase(outros);
+    const intro = chatBot.randomPhrase(chatBot.intros);
+    const activity = chatBot.randomPhrase(chatBot.activities);
+    const outro = chatBot.randomPhrase(chatBot.outros);
     console.log(intro + ' ' + activity + '... ' + outro + '!');
     rl.close();
 }
